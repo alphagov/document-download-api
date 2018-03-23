f0c313590ceac33e5b5226ecde5275a72f2f49f9
Alexey Bezhan <al@fueledbylemons.com>
2018-03-23 10:38:46 +0000

    Scan uploaded document with clamd

--- app/upload/views.py
+++ app/upload/views.py
@@ -1,3 +1,4 @@
+import clamd
 from flask import Blueprint, jsonify, request, url_for
 
 from app import document_store
@@ -12,6 +13,13 @@ upload_blueprint.before_request(check_auth)
 def upload_document(service_id):
     if 'document' not in request.files:
         return jsonify(error='No document upload'), 400
+
+    cd = clamd.ClamdUnixSocket()
+    virus_scan_result = cd.instream(request.files['document'])
+    if virus_scan_result['stream'][0] != 'OK':
+        return jsonify(error="Virus scan failed", scan_result=virus_scan_result['stream']), 400
+
+    request.files['document'].stream.seek(0)
 
     document = document_store.put(service_id, request.files['document'])
 
