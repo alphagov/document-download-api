655046ef8ae4efbfd103b95f6329a7b6453f32c4
Alexey Bezhan <al@fueledbylemons.com>
2018-04-16 17:19:17 +0100

    Add Prometheus metrics endpoint with gds-metrics
    Adds a /metrics endpoint that collects request times, codes and
    errors and can be scraped by Prometheus.
    
    This moves gunicorn configuration to a separate file to support
    prometheus client multiprocess mode.

---
+++ gunicorn_config.py
@@ -0,0 +1,12 @@
+import os
+
+from gds_metrics.gunicorn import child_exit # noqa
+
+bind = "0.0.0.0:{}".format(os.getenv("PORT"))
+
+workers = 4
+
+worker_class = "eventlet"
+worker_connections = 1000
+
+errorlog = "/home/vcap/logs/gunicorn_error.log"
