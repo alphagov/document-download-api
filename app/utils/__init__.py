84b728e460728257fb86e414f6356a85fb211c49
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2020-01-16 17:57:08 +0000

    make testing for docx files more robust
    some versions of `libmagic` report that docx files are actually of type
    `application/zip` rather than `application/vnd.openxmlformats....`.
    Which is technically true, they are zip archives. It appears that
    libmagic on paas isn't as up to date as that on my mac, so this commit
    adds code to do some manual checking.
    
    If it reports as a zip archive, then we try and check the zip headers to
    get the list of files (note, this doesn't actually uncompress anything).
    If there's a `word/document.xml` file, then that's good enough for us to
    assume it's a docx. This might lead to some weird false positives - the
    next step would involve extracting `[Content_Types].xml` and parsing the
    xml contained within - that's equivalent to what the libmagic library
    does currently - see https://github.com/threatstack/libmagic/blob/master/magic/Magdir/msooxml

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,10 +1,24 @@
+from zipfile import ZipFile, BadZipFile
+from io import BytesIO
+
 import magic
 
 
 def get_mime_type(document_stream):
-    try:
-        mime_type = magic.from_buffer(document_stream.read(1024), mime=True)
-    finally:
-        document_stream.seek(0)
+    data = document_stream.read()
+    document_stream.seek(0)
+
+    mime_type = magic.from_buffer(data, mime=True)
+
+    # some versions of libmagic mis-report docx as a zip file (which it technically is), so lets dive in and check
+    # the zip file headers to see if it looks like a docx.
+    if mime_type == 'application/zip':
+        try:
+            filenames = ZipFile(BytesIO(data)).namelist()
+            if 'word/document.xml' in filenames:
+                mime_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        except BadZipFile:
+            # dunno what this is, but it's not a zip and it's not an office format. octet-stream is a generic binary.
+            mime_type = 'application/octet-stream'
 
     return mime_type
