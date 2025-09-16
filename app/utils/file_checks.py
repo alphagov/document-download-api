fae29435cd47a1eadd66b1e5003b30586fc41ba4
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-09-16 15:29:22 +0100

    working example of cachable function
    Refactor Antivirus on doc upload
    
    Clean up
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP tests passing
    
    WIP tests passing
    
    clean up
    
    WIP
    
    WIP passing tests
    
    WIP passing tests refactored mimetype and av checks into single shared cachable function
    
    refactor into cachable method
    
    rename
    
    working example of cachable function

--- app/utils/file_checks.py
+++ app/utils/file_checks.py
@@ -1,8 +1,11 @@
+import mimetypes
+
 from flask import current_app
 from notifications_utils.clients.antivirus.antivirus_client import AntivirusError
 
 from app import antivirus_client
 from app.utils import get_mime_type
+from app.utils.files import split_filename
 
 
 class FiletypeError(Exception):
@@ -11,23 +14,34 @@ class FiletypeError(Exception):
         self.status_code = status_code
 
 
+def run_antivirus_and_mimetype_checks(file_data):
+    pass
+
+
 def run_antivirus_checks(file_data):
-    try:
-        virus_free = antivirus_client.scan(file_data)
-    except AntivirusError as e:
-        raise AntivirusError(message="Antivirus API error", status_code=503) from e
+    if current_app.config["ANTIVIRUS_ENABLED"]:
+        results = {}
+        try:
+            virus_free = antivirus_client.scan(file_data)
+        except AntivirusError:
+            return {"message": "Antivirus API error", "status_code": 503}
+        if virus_free:
+            results["virus_free"] = True
+        else:
+            results["virus_free"] = False
+        return results
+    return
 
-    if not virus_free:
-        raise AntivirusError(message="File did not pass the virus scan", status_code=400)
-    return virus_free
 
-
-def run_mimetype_checks(file_data, is_csv):
-    mimetype = get_mime_type(file_data)
-    # Our mimetype auto-detection sometimes resolves CSV content as text/plain, so we use
-    # an explicit POST body parameter `is_csv` from the caller to resolve it as text/csv
-    if is_csv and mimetype == "text/plain":
-        mimetype = "text/csv"
+def run_mimetype_checks(file_data, is_csv, filename=None):
+    if filename:
+        mimetype = mimetypes.types_map[split_filename(filename, dotted=True)[1]]
+    else:
+        mimetype = get_mime_type(file_data)
+        # Our mimetype auto-detection sometimes resolves CSV content as text/plain, so we use
+        # an explicit POST body parameter `is_csv` from the caller to resolve it as text/csv
+        if is_csv and mimetype == "text/plain":
+            mimetype = "text/csv"
     if mimetype not in current_app.config["MIME_TYPES_TO_FILE_EXTENSIONS"]:
         allowed_file_types = ", ".join(
             sorted({f"'.{x}'" for x in current_app.config["FILE_EXTENSIONS_TO_MIMETYPES"].keys()})
