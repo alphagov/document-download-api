4749bfa2ce51760abbe2759776b9755d73f6b732
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-09-24 13:33:16 +0100

    WIP tests passing

--- app/file_checks/views.py
+++ app/file_checks/views.py
@@ -1,11 +1,14 @@
+import mimetypes
 from base64 import b64decode, binascii
 from io import BytesIO
 
 from flask import Blueprint, abort, current_app, jsonify, request
 from werkzeug.exceptions import BadRequest
 
+from app.utils import get_mime_type
 from app.utils.authentication import check_auth
-from app.utils.file_checks import FiletypeError, run_antivirus_checks, run_mimetype_checks
+from app.utils.file_checks import run_antivirus_checks
+from app.utils.files import split_filename
 
 file_checks_blueprint = Blueprint("file_checks", __name__, url_prefix="")
 file_checks_blueprint.before_request(check_auth)
@@ -32,7 +35,7 @@ def _get_upload_document_request_data(data):
 
 
 @file_checks_blueprint.route("/antivirus_and_mimetype_check", methods=["POST"])
-def get_mime_type_and_run_antivirus_scan():
+def get_mime_type_and_run_antivirus_scan(filename=None):
     try:
         (
             file_data,
@@ -47,8 +50,24 @@ def get_mime_type_and_run_antivirus_scan():
     virus_free = virus_scan_results.get("virus_free")
     if not virus_free:
         return jsonify(error="File did not pass the virus scan"), 400
-    try:
-        mimetype = run_mimetype_checks(file_data, is_csv)
-    except FiletypeError as e:
-        return jsonify(error=e.error_message), e.status_code
+    # try:
+    #     mimetype = run_mimetype_checks(file_data, is_csv)
+    # except FiletypeError as e:
+    #     return jsonify(error=e.error_message), e.status_code
+    if filename:
+        mimetype = mimetypes.types_map[split_filename(filename, dotted=True)[1]]
+    else:
+        mimetype = get_mime_type(file_data)
+        # Our mimetype auto-detection sometimes resolves CSV content as text/plain, so we use
+        # an explicit POST body parameter `is_csv` from the caller to resolve it as text/csv
+        if is_csv and mimetype == "text/plain":
+            mimetype = "text/csv"
+    if mimetype not in current_app.config["MIME_TYPES_TO_FILE_EXTENSIONS"]:
+        allowed_file_types = ", ".join(
+            sorted({f"'.{x}'" for x in current_app.config["FILE_EXTENSIONS_TO_MIMETYPES"].keys()})
+        )
+        # raise FiletypeError(
+        #     error_message=f"Unsupported file type '{mimetype}'. Supported types are: {allowed_file_types}",
+        #     status_code=400,
+        return jsonify(error=f"Unsupported file type '{mimetype}'. Supported types are: {allowed_file_types}"), 400
     return jsonify(virus_free=virus_free, mimetype=mimetype), 200
