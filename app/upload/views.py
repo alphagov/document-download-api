df84853d112df32426f65a827269f5e472affeb0
Chris Hill-Scott <me@quis.cc>
2025-09-26 11:25:01 +0100

    Remove old function

--- app/upload/views.py
+++ app/upload/views.py
@@ -1,63 +1,14 @@
-from base64 import b64decode, binascii
-from io import BytesIO
 
-from flask import Blueprint, abort, current_app, jsonify, request
-from notifications_utils.recipient_validation.errors import InvalidEmailError
+from flask import Blueprint, jsonify, request
 from werkzeug.exceptions import BadRequest
 
 from app import document_store
 from app.file_checks.views import UploadedFile
 from app.utils.authentication import check_auth
 from app.utils.urls import get_direct_file_url, get_frontend_download_url
-from app.utils.validation import (
-    clean_and_validate_email_address,
-    clean_and_validate_retention_period,
-    validate_filename,
-)
 
 upload_blueprint = Blueprint("upload", __name__, url_prefix="")
 upload_blueprint.before_request(check_auth)
-
-
-def _get_upload_document_request_data(data):  # noqa: C901
-    if "document" not in data:
-        raise BadRequest("No document upload")
-
-    try:
-        raw_content = b64decode(data["document"])
-    except (binascii.Error, ValueError) as e:
-        raise BadRequest("Document is not base64 encoded") from e
-
-    if len(raw_content) > current_app.config["MAX_DECODED_FILE_SIZE"]:
-        abort(413)
-    file_data = BytesIO(raw_content)
-    is_csv = data.get("is_csv", False)
-
-    if not isinstance(is_csv, bool):
-        raise BadRequest("Value for is_csv must be a boolean")
-
-    confirmation_email = data.get("confirmation_email", None)
-    if confirmation_email is not None:
-        try:
-            confirmation_email = clean_and_validate_email_address(confirmation_email)
-        except InvalidEmailError as e:
-            raise BadRequest(str(e)) from e
-
-    retention_period = data.get("retention_period", None)
-    if retention_period is not None:
-        try:
-            retention_period = clean_and_validate_retention_period(retention_period)
-        except ValueError as e:
-            raise BadRequest(str(e)) from e
-
-    filename = data.get("filename", None)
-    if filename:
-        try:
-            filename = validate_filename(filename)
-        except ValueError as e:
-            raise BadRequest(str(e)) from e
-
-    return file_data, is_csv, confirmation_email, retention_period, filename
 
 
 @upload_blueprint.route("/services/<uuid:service_id>/documents", methods=["POST"])
