1d76ae2a89b5e010b713fd0658ac5f89bc1c2667
Chris Hill-Scott <me@quis.cc>
2025-09-26 12:54:42 +0100

    Remove unused code

--- app/utils/file_checks.py
+++ app/utils/file_checks.py
@@ -158,37 +158,3 @@ class UploadedFile:
             )
         return mimetype
 
-
-def run_antivirus_checks(file_data):
-    if current_app.config["ANTIVIRUS_ENABLED"]:
-        results = {}
-        try:
-            virus_free = antivirus_client.scan(file_data)
-        except AntivirusError:
-            return {"message": "Antivirus API error", "status_code": 503}
-        if virus_free:
-            results["virus_free"] = True
-        else:
-            results["virus_free"] = False
-        return results
-    return
-
-
-def run_mimetype_checks(file_data, is_csv, filename=None):
-    if filename:
-        mimetype = mimetypes.types_map[split_filename(filename, dotted=True)[1]]
-    else:
-        mimetype = get_mime_type(file_data)
-        # Our mimetype auto-detection sometimes resolves CSV content as text/plain, so we use
-        # an explicit POST body parameter `is_csv` from the caller to resolve it as text/csv
-        if is_csv and mimetype == "text/plain":
-            mimetype = "text/csv"
-    if mimetype not in current_app.config["MIME_TYPES_TO_FILE_EXTENSIONS"]:
-        allowed_file_types = ", ".join(
-            sorted({f"'.{x}'" for x in current_app.config["FILE_EXTENSIONS_TO_MIMETYPES"].keys()})
-        )
-        raise FiletypeError(
-            error_message=f"Unsupported file type '{mimetype}'. Supported types are: {allowed_file_types}",
-            status_code=400,
-        )
-    return mimetype
